fb426be5eb2c7d6275f628ba4809ae7bdd484ee0
张涛 <zhangtao@hujiang.com>
2017-03-02 15:21:14 +0800

    px2dp

---
+++ px2dp-sw.js
@@ -0,0 +1,82 @@
+var cacheName = 'px-to-dp-v1.1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/ic_about_white.svg',
+  '/images/ic_back_white.svg',
+  '/images/offline.jpg',
+  '/images/woodwall.jpg',
+  '/images/woodwall2.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {  
+  e.waitUntil(  
+    caches.keys().then(function(keyList) {  
+      return Promise.all(keyList.map(function(key) {   
+        if (key !== cacheName) {  
+          return caches.delete(key);  
+        }  
+      }));  
+    })  
+  );  
+});
+
+self.addEventListener('fetch', function(e) {
+  var extendDataUrl = [
+    '/download.json'
+  ];
+
+  var allDataUrl = extendDataUrl;
+  var requestIsDataApi = false;
+
+  //如果是 API 请求，先网络后缓存
+  for (count in extendDataUrl){
+    if (e.request.url.indexOf(extendDataUrl[count]) > -1 ) {
+      requestIsDataApi = true;
+      e.respondWith(
+        fetch(e.request)
+        .then(function(response) {
+          return caches.open(cacheName).then(function(cache){
+            cache.put(e.request.url, response.clone()); 
+            return response;
+          });
+        })
+        .catch(function(){
+          return caches.match(e.request.url);
+        })
+      );
+      break;
+    }
+  }
+
+  //一般资源请求，先缓存再网络再默认
+  if (!requestIsDataApi){
+    e.respondWith(
+      caches.match(e.request).then(function(respond){
+        return respond || fetch(e.request)
+          .then(function(res){
+            return caches.open(cacheName).then(function(cache){
+              cache.put(e.request.url, res.clone()); 
+              return res;
+            });
+          })
+          .catch(function(){
+            return caches.match('offline.html');
+          });
+      })
+    )
+  }
+
+});
+
---
+++ scripts/px2dp.js
@@ -0,0 +1,61 @@
+(function() {
+  'use strict';
+
+  var app = { 
+    container: document.querySelector('.post-list-body'),
+  };
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  document.getElementById('butPlus').addEventListener('click', function() {
+    app.addItemContent();
+  });
+
+  document.getElementById('butCompute').addEventListener('click', function() {
+    var scale = app.scale(widthAndHeight.mWidth.value, widthAndHeight.mHeight.value);
+    var editTextList = app.container.getElementsByTagName('input');
+    var textList = app.container.getElementsByTagName('font');
+    for(var i = 0; i < editTextList.length; i++){
+      textList[i].innerHTML = (editTextList[i].value*scale);
+    }
+  });
+
+  /*****************************************************************************
+   *
+   * Methods to update/refresh the UI
+   *
+   ****************************************************************************/
+
+  app.scale = function(width, height){
+    if (width > height){
+      var i = width;
+      width = height;
+      height = i;
+    }
+
+    return (height / 1920);
+  }   
+
+  app.createItem = function(){
+    var template = document.querySelector('#itemTemplate').content;
+    var item = template.cloneNode(true);
+    return item;
+  };
+
+  app.addItemContent = function(){
+    var item = app.createItem();
+    app.container.appendChild(item);
+  };
+
+  app.addItemContent();
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/px2dp-sw.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+
+})();
